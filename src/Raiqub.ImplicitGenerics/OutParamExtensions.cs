a3cd6cbbf476ebdfa0cdbd409ce09b51ac7a0e89
Fabricio Godoy <fabricio.godoy@ciandt.com>
2022-07-26 15:10:53 -0300

    Support .NET Standard 1.0

--- src/Raiqub.ImplicitGenerics/OutParamExtensions.cs
+++ src/Raiqub.ImplicitGenerics/OutParamExtensions.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 
 namespace Raiqub.ImplicitGenerics;
 
@@ -9,15 +10,18 @@ namespace Raiqub.ImplicitGenerics;
 [EditorBrowsable(EditorBrowsableState.Never)]
 public static class OutParamExtensions
 {
+#pragma warning disable CS8777 // Parameter must have a non-null value when exiting
     /// <summary>Displays a debug message if <paramref name="outType"/> is null.</summary>
     /// <param name="outType">The instance to validate as non-null.</param>
     /// <typeparam name="T">The generic type.</typeparam>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     [Conditional("DEBUG")]
+    [ContractAnnotation("outType:null=>halt")]
     public static void DebugIfNull<T>([NotNull] this IOutParam<T>? outType)
     {
         Debug.Assert(outType is not null);
     }
+#pragma warning restore CS8777
 
     /// <summary>
     /// Throws an <see cref="ArgumentNullException"/> if <paramref name="outType"/> is null.
@@ -26,6 +30,7 @@ public static class OutParamExtensions
     /// <param name="paramName">The name of the parameter with which argument corresponds.</param>
     /// <typeparam name="T">The generic type.</typeparam>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [ContractAnnotation("outType:null=>halt")]
     public static void ThrowIfNull<T>(
         [NotNull] this IOutParam<T>? outType,
         [CallerArgumentExpression("outType")] string? paramName = null)
